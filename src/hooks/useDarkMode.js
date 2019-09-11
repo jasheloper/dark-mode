13fcc82a9eadbd63ad7141da00ce5d8ab281dbd2
Jashele T <jashele@Jasheles-Air.domain>
2019-09-10 20:28:55 -0700

    set up the dark mode function

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -6,12 +6,29 @@ import { useLocalStorage } from './useLocalStorage.js';
 
 export default function useDarkMode(body) {
 
-    const [darkmode, setDarkMode] = useLocalStorage('not enabled');
+const [darkmode, setDarkMode] = useLocalStorage('dark');
 
 
-    useEffect(() => {
-       
-      });
+useEffect(() => {
+
+// check to see if the value from useLocalStorage is true or false.
+// If it's true, add the class dark-mode to the body element.
+// If it's false, remove the class from the body element.
+
+
+{darkmode ? document.body.classList.add('dark-mode__toggle') : document.body.classList.remove('dark-mode__toggle'); }
+
+
+}, []);
+
+const setMode = light => {
+setDarkMode(light);
+
+window.localStorage.setItem(dark, JSON.stringify(light));
+}
+
+
+return [darkmode, setMode]
 }
 
 
